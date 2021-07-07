aeefa8ef302f217c83077d095759e4a9467ba396
dusgn <33680794+gilthub@users.noreply.github.com>
2021-07-07 18:48:27 +0900

    Initial commit

---
+++ src/main/java/me/dusgn/springbootnextlevel/SpringbootnextlevelApplication.java
@@ -0,0 +1,13 @@
+package me.dusgn.springbootnextlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootnextlevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootnextlevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dusgn/springbootnextlevel/SpringbootnextlevelApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dusgn.springbootnextlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootnextlevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
